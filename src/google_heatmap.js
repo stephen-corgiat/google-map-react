2dcbf64ce90117a2cf1c5bbf340b4dea42270b38
Stephen Corgiat <nitelord@gmail.com>
2018-02-20 07:57:02 -0800

    Add support for weight parameter

--- src/google_heatmap.js
+++ src/google_heatmap.js
@@ -3,9 +3,10 @@ import fp from 'lodash/fp';
 export const generateHeatmap = (instance, { positions }) =>
   new instance.visualization.HeatmapLayer({
     data: fp.reduce(
-      (acc, { lat, lng }) => {
+      (acc, { lat, lng, weight }) => {
         acc.push({
           location: new instance.LatLng(lat, lng),
+          weight: weight
         });
         return acc;
       },
